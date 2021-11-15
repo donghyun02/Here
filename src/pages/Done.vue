7dfcb11a5fb00cc3e9f84aa523356c3a7c4d190a
donghyun02 <dhkim20157@gmail.com>
2021-11-15 19:05:35 +0900

    CHG: 스타일 변경

--- src/pages/Done.vue
+++ src/pages/Done.vue
@@ -2,15 +2,42 @@
   <div class="done">
       <navigation />
       <div class="done__content">
-        <h1>예약 확정</h1>
+        <h2 class="done__title">예약 확정</h2>
+        <p class="done__subtitle">고객님의 예약 내역입니다.</p>
         <h5 class="done__category"><strong>식당 정보</strong></h5>
-        <p>식당명: 말랑 파스타</p>
-        <p>날짜: 2021년 11월 10일</p>
-        <p>시간: 17:00</p>
-        <p>좌석: 창가 좌석</p>
+        <div class="done__text-set">
+          <p class="done__item-title">식당명</p>
+          <p class="done__text">63빌딩 레스토랑</p>
+        </div>
+
+        <div class="done__text-set">
+          <p class="done__item-title">날짜</p>
+          <p class="done__text">2021년 11월 11일</p>
+        </div>
+
+        <div class="done__text-set">
+          <p class="done__item-title">시간</p>
+          <p class="done__text">17:00</p>
+        </div>
+
+        <div class="done__text-set">
+          <p class="done__item-title">좌석</p>
+          <p class="done__text">창가 좌석</p>
+        </div>
+
+        <div class="done__separator"></div>
+
         <h5 class="done__category"><strong>예약자 정보</strong></h5>
-        <p>성명: 구민영</p>
-        <p>전화번호: 000-1111-2222</p>
+
+        <div class="done__text-set">
+          <p class="done__item-title">성명</p>
+          <p class="done__text">구민영</p>
+        </div>
+
+        <div class="done__text-set">
+          <p class="done__item-title">전화번호</p>
+          <p class="done__text">000-1111-2222</p>
+        </div>
       </div>
   </div>
 </template>
@@ -37,12 +64,48 @@ export default {
   align-items: center;
 }
 
+.done__title {
+  width: 100%;
+}
+
+.done__subtitle {
+  width: 100%;
+  margin-bottom: 30px;
+  opacity: 0.6;
+}
+
 .done__content {
   width: 900px;
   margin-top: 60px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 .done__category {
-  margin-top: 40px;
+  width: 500px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.done__text-set {
+  width: 500px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.done__item-title {
+  font-weight: bold;
+}
+
+.done__separator {
+  width: 500px;
+  height: 1px;
+  margin-top: 4px;
+
+  background-color: #DEE2E5;
 }
 </style>
