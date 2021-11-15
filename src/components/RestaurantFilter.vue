9a26009c9577bcb51958779f174e8123b7d2631e
donghyun02 <dhkim20157@gmail.com>
2021-11-15 19:37:05 +0900

    ADD: filter component

---
+++ src/components/RestaurantFilter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="filter">
+    <b-button v-b-toggle.collapse-1 variant="primary">필터<b-icon-filter /></b-button>
+    <b-collapse id="collapse-1" class="mt-2">
+      <b-card>
+        <b-button class="filter__button" size="sm">전체</b-button>
+        <b-button class="filter__button" size="sm">뷔페</b-button>
+        <b-button class="filter__button" size="sm">레스토랑</b-button>
+        <b-button class="filter__button" size="sm">일반식당</b-button>
+      </b-card>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+import { BIconFilter } from 'bootstrap-vue'
+
+export default {
+  name: 'RestaurantFilter',
+  components: {
+    BIconFilter,
+  },
+}
+</script>
+
+<style lang="css" scoped>
+.filter {
+  width: 100%;
+}
+
+.filter__button:not(:first-of-type) {
+  margin-left: 10px;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,28 +1,30 @@
 <template>
-    <div class="index">
-        <navigation />
-        <div class="index__content" v-if="!isFetched">
-            <restaurant-card-skeleton class="index__card" />
-            <restaurant-card-skeleton class="index__card" />
-            <restaurant-card-skeleton class="index__card" />
-        </div>
-        <div class="index__content" v-else>
-            <restaurant-card
-                class="index__card"
-                v-for="restaurant in restaurants"
-                :key="restaurant.id"
-                :id="restaurant.id"
-                :name="restaurant.name"
-                :imageURL="restaurant.image"
-                :rating="restaurant.rating"
-                :address="restaurant.address"
-            />
-        </div>
+  <div class="index">
+    <navigation />
+    <div class="index__content" v-if="!isFetched">
+      <restaurant-card-skeleton class="index__card" />
+      <restaurant-card-skeleton class="index__card" />
+      <restaurant-card-skeleton class="index__card" />
     </div>
+    <div class="index__content" v-else>
+      <restaurant-filter class="index__filter" />
+      <restaurant-card
+        class="index__card"
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+        :id="restaurant.id"
+        :name="restaurant.name"
+        :imageURL="restaurant.image"
+        :rating="restaurant.rating"
+        :address="restaurant.address"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import Navigation from '@/components/Navigation.vue'
+import RestaurantFilter from '@/components/RestaurantFilter.vue'
 import RestaurantCard from '@/components/RestaurantCard.vue'
 import RestaurantCardSkeleton from '@/skeletons/RestaurantCardSkeleton.vue'
 import { createNamespacedHelpers } from 'vuex'
@@ -30,40 +32,55 @@ import { createNamespacedHelpers } from 'vuex'
 const { mapState, mapActions } = createNamespacedHelpers('RestaurantsModule')
 
 export default {
-    name: 'Index',
-    components: {
-        Navigation,
-        RestaurantCard,
-        RestaurantCardSkeleton,
-    },
-    computed: {
-        ...mapState({
-            restaurants: 'restaurants',
-            isFetched: 'isFetched',
-        }),
-    },
-    methods: {
-        ...mapActions([
-            'fetchRestaurants',
-        ]),
-    },
-    created() {
-        this.fetchRestaurants()
-    },
+  name: 'Index',
+  components: {
+    Navigation,
+    RestaurantFilter,
+    RestaurantCard,
+    RestaurantCardSkeleton,
+  },
+  computed: {
+    ...mapState({
+      restaurants: 'restaurants',
+      isFetched: 'isFetched',
+    }),
+  },
+  methods: {
+    ...mapActions([
+        'fetchRestaurants',
+    ]),
+  },
+  created() {
+    this.fetchRestaurants()
+  },
 }
 </script>
 
 <style scoped>
-.index__content {
-    margin: 20px;
+.index {
+  width: 100%;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+.index__content {
+  max-width: 900px;
+  width: 100%;
+  margin: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.index__filter {
+  width: 100%;
 }
 
 .index__card {
-    margin-top: 10px;
+  margin-top: 10px;
 }
 </style>
